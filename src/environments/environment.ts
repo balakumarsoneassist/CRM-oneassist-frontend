15ae358fcbfae19fb5260efabe7254d256ca17ca
Sowmiyasrioneassist <sowmiyasri.c@oneassist.net>
2026-02-02 21:51:10 +0530

    url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000', // Development API endpoint
+  // apiUrl: 'http://localhost:3000', // Development API endpoint
   //  apiUrl: 'http://31.97.237.60:3000', // production API endpoint
   // qrBaseUrl: 'http://oneassist.net.in/newloan.html', // QR code base URL
   // qrBaseUrl: 'http://localhost/qrcode/qrloann.html', // QR code base URL
   // production: true,
-  //apiUrl: 'https://oneassist.net.in/crmapi', // Update with your production domain
+  apiUrl: 'https://oneassist.net.in/crmapi', // Update with your production domain
   qrBaseUrl: 'https://oneassist.net.in/qrcode/qrloann.html', // Update with your production domain
   //ng build --configuration production --base-href /crm/
 };
